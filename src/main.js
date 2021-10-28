ced5febe24d1d848962bb58edb8ffb9b9cff4ea1
Yasya Smielova <blackcat90inc@gmail.com>
2021-10-28 09:15:34 +0300

    remove unused package

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import '@babel/polyfill'
 import 'mutationobserver-shim'
 import { createApp } from 'vue'
 import BootstrapVue3 from 'bootstrap-vue-3'
-import VueMask from '@devindex/vue-mask';
 import { store } from './store/index'
 import router from './router'
 import App from './App.vue'
@@ -12,7 +11,6 @@ import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
 const app = createApp(App)
 app.use(BootstrapVue3)
-app.use(VueMask)
 app.use(store)
 app.use(router)
 app.mount('#app')
